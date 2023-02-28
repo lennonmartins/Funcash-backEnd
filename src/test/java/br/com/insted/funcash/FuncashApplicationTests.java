81255311cb344b3eec2b4d7de39770f12b62e26f
Lennon <lennonmcarlos@gmail.com>
2023-02-28 20:38:25 -0300

    subindo base do projeto

---
+++ src/test/java/br/com/insted/funcash/FuncashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insted.funcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
