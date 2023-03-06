20a1e053a0d21aae2df6b15700de3cbe932ab178
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-03-06 17:46:33 -0400

    att

--- src/test/java/br/com/insted/funcash/controller/CrincaControllerTest.java
+++ src/test/java/br/com/insted/funcash/controller/CrincaControllerTest.java
@@ -1,30 +1,28 @@
 package br.com.insted.funcash.controller;
 
-import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.http.MediaType;
+
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-
-import br.com.insted.funcash.builders.CriancaBuilder;
-import br.com.insted.funcash.dto.CriancaRequestDTO;
-import br.com.insted.funcash.models.Crianca;
-import br.com.insted.funcash.repository.CriancaRepository;
-import net.minidev.json.JSONUtil;
-import static org.hamcrest.Matchers.containsString;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
+import br.com.insted.funcash.builders.CriancaBuilder;
+import br.com.insted.funcash.models.Crianca;
+import br.com.insted.funcash.repository.CriancaRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
