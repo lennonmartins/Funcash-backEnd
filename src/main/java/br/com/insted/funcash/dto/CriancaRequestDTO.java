00e9ddfad341e34770f0919eed59d22ba4f6f44d
Lennon <lennonmcarlos@gmail.com>
2023-04-02 17:10:32 -0400

    ajuste dos teste anteriores e inclusao da dependência jackson-datatype

--- src/main/java/br/com/insted/funcash/dto/CriancaRequestDTO.java
+++ src/main/java/br/com/insted/funcash/dto/CriancaRequestDTO.java
@@ -1,6 +1,5 @@
 package br.com.insted.funcash.dto;
 
-import br.com.insted.funcash.models.Crianca;
 import br.com.insted.funcash.models.Genero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,15 +17,5 @@ public class CriancaRequestDTO {
     private String nome;
     private String apelido;
     private Genero genero;
-
-    public CriancaRequestDTO(Crianca crianca) {
-        this.dataDeNascimento = dataDeNascimento;
-        this.email = email;
-        this.senha = senha;
-        this.saldo = saldo;
-        this.nome = nome;
-        this.apelido = apelido;
-        this.genero = genero;
-    }
 
 }
--- src/main/java/br/com/insted/funcash/utils/JsonUtil.java
+++ src/main/java/br/com/insted/funcash/utils/JsonUtil.java
@@ -1,26 +1,23 @@
 package br.com.insted.funcash.utils;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtil {
 
-    // public static byte[] toJson(Object object) throws JsonProcessingException{
-    // ObjectMapper mapper = new ObjectMapper();
-    // mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    // return mapper.writeValueAsBytes(object);
-    // }
+    public static byte[] toJson(Object object) throws JsonProcessingException{
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    return mapper.writeValueAsBytes(object);
+    }
 
     public static <T> T mapFomJson(String json, Class<T> clazz) throws JsonMappingException, JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.readValue(json, clazz);
-    }
-
-    JsonMapper jsonMapper = new JsonMapper();
-   
+    } 
 
 }
