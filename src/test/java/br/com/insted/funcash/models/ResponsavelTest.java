d473eb5f48553b958c595559d05680dc10a9a6d3
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-04-19 14:30:02 -0400

    foto_responsavel

--- src/test/java/br/com/insted/funcash/models/ResponsavelTest.java
+++ src/test/java/br/com/insted/funcash/models/ResponsavelTest.java
@@ -1,5 +1,33 @@
 package br.com.insted.funcash.models;
 
+import java.time.LocalDate;
+
+import br.com.insted.funcash.utils.ArquivoUtils;
+
+@SpringBootTest
 public class ResponsavelTest {
-    
+    private byte[] icone;
+
+    @BeforeEach
+    public void carregarIcone() throws IOException {
+        this.icone = ArquivoUtils.abrirArquivo("src\\test\\java\\br\\com\\insted\\funcash\\icons\\Responsavel_Foto.svg");
+    }
+
+    @Test 
+    public void deve_criar_um_tipo() throws FileNotFoundException, IOException {
+        // Arrange
+        String nome = "Water";
+        String email = "#4592c4";
+        String cpf = "";
+        LocalDate data
+        byte[] foto = this.icone;
+
+        // Action
+        Tipo tipo = new TipoBuilder().construir();
+
+        // Assert
+        Assertions.assertEquals(nome, tipo.getNome());
+        Assertions.assertEquals(cor, tipo.getCor());
+        Assertions.assertEquals(Arrays.toString(simbolo), Arrays.toString(tipo.getSimbolo()));
+    }
 }
