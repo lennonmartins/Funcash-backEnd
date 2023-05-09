65354bef34a135e3d71f7eb9ebbe10e99cc9879b
Lennon <lennonmcarlos@gmail.com>
2023-05-09 11:46:58 -0400

    adicção da anotação cors

--- src/main/java/br/com/insted/funcash/controller/TarefaController.java
+++ src/main/java/br/com/insted/funcash/controller/TarefaController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = {"api/v1/tarefas"}, produces ={"application/json"})
 public class TarefaController {
     private final TarefaService tarefaService;
