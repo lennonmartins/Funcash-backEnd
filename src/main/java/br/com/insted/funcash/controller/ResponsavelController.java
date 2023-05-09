e55921e6c626d024016381d823dd42ea61bdb62e
Lennon <lennonmcarlos@gmail.com>
2023-05-09 00:20:44 -0400

    Adição da propriedade de Crossorigin no controler do Responsável

--- src/main/java/br/com/insted/funcash/controller/ResponsavelController.java
+++ src/main/java/br/com/insted/funcash/controller/ResponsavelController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = {"/api/v1/responsavel"}, produces = {"application/json"})
 public class ResponsavelController {
     private final ResponsavelService responsavelService;
