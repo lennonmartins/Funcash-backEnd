dae48e136f7cad2d0ee4c7c18564248c2e43f338
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-06-12 16:47:04 -0400

    login responsavel

--- src/main/java/br/com/insted/funcash/controller/AuthController.java
+++ src/main/java/br/com/insted/funcash/controller/AuthController.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.insted.funcash.dto.ResponsavelResponseDTO;
 import br.com.insted.funcash.service.AuthService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -20,13 +21,17 @@ public class AuthController {
 
     @Autowired
     AuthService authService;
-    
-    @PostMapping(path = {"/entrar"},consumes = { "application/json" })
-    public ResponseEntity<ResponsavelResponseDTO> autentitcar(@Valid @RequestBody LoginDTO loginRequest ){
-        System.out.println("------------------------------------");
-        System.out.println("emial: " + loginRequest.getEmail());
-        System.out.println("senha: " + loginRequest.getSenha());
-        System.out.println("------------------------------------");
-       return ResponseEntity.ok(authService.loginResponsavel(loginRequest.getEmail(), loginRequest.getSenha()));
+    @PostMapping(path = {"/entrar"})
+    public ResponseEntity<ResponsavelResponseDTO> autenticar(@Valid @RequestBody LoginDTO loginRequest) {
+        ResponsavelResponseDTO responseDTO = authService.loginResponsavel(loginRequest.getEmail(), loginRequest.getSenha());
+        return ResponseEntity.ok(responseDTO);
     }
+    // @PostMapping(path = {"/entrar"},consumes = { "application/json" })
+    // public ResponseEntity<ResponsavelResponseDTO> autentitcar(@Valid @RequestBody LoginDTO loginRequest ){
+    //     System.out.println("------------------------------------");
+    //     System.out.println("emial: " + loginRequest.getEmail());
+    //     System.out.println("senha: " + loginRequest.getSenha());
+    //     System.out.println("------------------------------------");
+    //    return ResponseEntity.ok(authService.loginResponsavel(loginRequest.getEmail(), loginRequest.getSenha()));
+    // }
 }
--- src/test/java/br/com/insted/funcash/services/AuthServiceTest.java
+++ src/test/java/br/com/insted/funcash/services/AuthServiceTest.java
@@ -1,15 +1,37 @@
 package br.com.insted.funcash.services;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import br.com.insted.funcash.models.Responsavel;
-
+import br.com.insted.funcash.models.Responsavel;
+import br.com.insted.funcash.repository.ResponsavelRepository;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 @DataJpaTest
 public class AuthServiceTest {
-    
+    @Autowired
+    private ResponsavelRepository responsavelRepository;
+
     @Test
-    void deve_buscar_um_responsavel_por_email_e_senha(){
-        
+    public void testEncontrarPorEmailESenha() {
+        // Dados de teste
+        String email = "seu_email";
+        String senha = "sua_senha";
+
+        // Realiza a busca
+        Optional<Responsavel> responsavelOptional = responsavelRepository.encontrarPorEmailESenha(email, senha);
+
+        // Verifica se o resultado foi encontrado
+        assertTrue(responsavelOptional.isPresent(), "Usuário responsável não encontrado");
+
+        // Obtém o objeto Responsavel do resultado
+        Responsavel responsavel = responsavelOptional.get();
+
+        // Verifique se o email e a senha correspondem aos dados de teste
+        assertEquals(email, responsavel.getEmail(), "Email não corresponde");
+        assertEquals(senha, responsavel.getSenha(), "Senha não corresponde");
     }
 }
