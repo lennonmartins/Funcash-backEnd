0d1abb014f7f228f7d063cd76ed1e4aaf925c6e6
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-08-02 17:34:16 -0400

    Criando Entidade Base

---
+++ src/main/java/br/com/insted/funcash/utils/EntidadeBase.java
@@ -0,0 +1,17 @@
+package br.com.insted.funcash.utils;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class EntidadeBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+}
