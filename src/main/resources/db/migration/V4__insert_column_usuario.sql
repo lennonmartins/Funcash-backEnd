402e553005aa59db27d4aae5cbf264485f9c20eb
Lennon Martins <lennonmartins@digix.com.br>
2023-08-24 12:17:20 -0400

    inclusão do script para alteração

--- src/main/resources/db/migration/V4__insert_column_usuario.sql
+++ src/main/resources/db/migration/V4__insert_column_usuario.sql
@@ -3,3 +3,15 @@ add crianca_id numeric(19,0)
 
 alter table dbo.usuario
 add constraint usuario_crianca_fk foreign key (crianca_id) references dbo.crianca (id);
+
+
+Begin transaction;
+Begin try
+    insert into usuario(email, senha, crianca_id)
+    select c.email, c.senha, c.id
+    From Crianca c;
+    commit
+end try
+begin catch
+    rollback
+end catch;
