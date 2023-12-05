db13a923e552c4177f22188cc0399f2ccbab20e5
lennonmartins <lennonmcarlos@gmail.com>
2023-12-04 22:14:17 -0400

    ajustes para receber merge.

--- target/funcash/WEB-INF/classes/db/migration/V7__drop_columns_responsavel.sql
+++
@@ -1,5 +0,0 @@
-alter table dbo.responsavel
-drop column email;
-
-alter table dbo.responsavel
-drop column senha;
--- target/funcash/WEB-INF/classes/db/migration/V8__drop_columns_crianca.sql
+++
@@ -1,5 +0,0 @@
-alter table dbo.crianca
-drop column email;
-
-alter table dbo.crianca
-drop column senha;
